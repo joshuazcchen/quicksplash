710dda62d337223942fc0a159a9d7dc1b33484cb
joshua <joshuaz.chen@mail.utoronto.ca>
2026-03-29 19:56:05 -0400

    fixed packet logic bc sending a strcut doesnt send a variable size lol

--- a3/src/server/lobby.c
+++ a3/src/server/lobby.c
@@ -26,8 +26,8 @@ void start_lobby(int listenfd) {
 
     while (1) {
         read_fds = fds;
-        // TODO: error check this
-        select(max_fd + 1, &read_fds, NULL, NULL, NULL);
+        // TODO: error check this properly
+        if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) < 0) { continue; }
 
         // manages new connection attempts
         if (FD_ISSET(listenfd, &read_fds)) {
@@ -48,16 +48,35 @@ void start_lobby(int listenfd) {
         }
 
         // listen for packets
+        int ret;
         for (int i = 0; i < LOBBY_SIZE; i++) {
             if (players[i].fd != -1 && FD_ISSET(players[i].fd, &read_fds)) {
-                if (game_read(&players[i]) == 1) {
-                    printf("test buffer success, buffer read from %d of type %d %s\n", players[i].fd, players[i].partial.type, players[i].partial.data);
-                } else {
-                    printf("test buffer failed or not finished %d", players[i].fd);
-                    close(players[i].fd);
-                    FD_CLR(players[i].fd, &fds);
-                    players[i].fd = -1;
+                while ((ret = game_read(&players[i])) == 1) {
+                    printf("big %d", ret);
+                    if (players[i].ready) {
+                        printf("YOOOOO SUCCESS! PACKET IS READY! user %d type %d data %s\n", i, players[i].partial.type, players[i].partial.data);
+                        players[i].ready = 0;
+                    } else if (ret < 0) {
+                        printf("test buffer failed %d\n", players[i].fd);
+                        close(players[i].fd);
+                        FD_CLR(players[i].fd, &fds);
+                        players[i].fd = -1;
+                    }
                 }
+
+//                if ((ret = game_read(&players[i])) == 1) {
+//                    printf("test buffer success, buffer read from %d of type %d %s\n", players[i].fd, players[i].partial.type, players[i].partial.data);
+//                    if (players[i].partial.type == 0) {
+//                        
+//                    }
+//                } else if (ret == 0) {
+//                    printf("read yes buffer not full %d %d %s\n", players[i].fd, players[i].partial.type, players[i].partial.data);
+//                } else {
+//                    printf("test buffer failed %d\n", players[i].fd);
+//                    close(players[i].fd);
+//                    FD_CLR(players[i].fd, &fds);
+//                    players[i].fd = -1;
+//                }
             }
         }
     }
