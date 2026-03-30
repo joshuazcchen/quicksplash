8f7e5104e23522cba02e840f8ad12e284da8c964
joshua <joshuaz.chen@mail.utoronto.ca>
2026-03-30 00:17:55 -0400

    added gitignore, and also a comment into init.c

--- src/client/init.c
+++ src/client/init.c
@@ -7,5 +7,6 @@ void start_lobby(int listenfd); // TODO: move this to a header
 
 int main() {
     struct sockaddr_in socket = init_server_addr(30000); 
-    int listenfd = socketfd
+    int listenfd = socketfd;
+    // i dont think i ended up using this at all lol
 }
