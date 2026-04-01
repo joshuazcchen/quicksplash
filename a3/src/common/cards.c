7005e6d34f80545459364dbdfbaad4c0ada7f498
erikhateshw <erik.dadashyan@gmail.com>
2026-03-31 23:50:52 -0400

    cards functional

--- a3/src/common/cards.c
+++ a3/src/common/cards.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <time.h> 
 #include "../../include/gamestructs.h"
+
+
 
 Card ** generate_cards(FILE * file){
     char prompt[MAX_PROMPT_SIZE]; 
@@ -21,13 +23,12 @@ Card ** generate_cards(FILE * file){
             perror("malloc");
             exit(1);
         }
-        printf("prompt size: %d \n",(int)strlen(prompt));
         cards[i]->prompt_text = malloc(sizeof(char)*(strlen(prompt))); // already size + 1 because of newline char
         if (!cards[i]->prompt_text) {
             perror("malloc");
             exit(1);
         }
-        strncpy(cards[i]->prompt_text, prompt,(strlen(prompt))); // need to malloc so pointer
+        strncpy(cards[i]->prompt_text, prompt,(strlen(prompt))); // copy over data written in prompt read from file
         (cards[i]->prompt_text)[(strlen(prompt)-1)] = '\0'; // replace newline char with null term
         i++; 
     }
@@ -42,13 +43,16 @@ void free_cards(Card ** cards){
     }
     free(cards);
 }
-
-Card ** draw_n_cards(int n){
-    return 1; 
+// draws a random card
+Card* draw_random(Card** cards){
+    int rand_index = rand() % (PROMPT_COUNT-1); // generate random number between 0 and PROMPT_COUNT
+    printf("the random number i rolled is: %d \n ", rand_index);
+    return  cards[rand_index];
 }
 
 int main(){
     FILE *prompt_file; 
+    srand(time(NULL));
 
     prompt_file = fopen("../../assets/prompts.txt","r");
     if (!prompt_file){
@@ -56,11 +60,20 @@ int main(){
     }
 
     Card ** cards = generate_cards(prompt_file);
-    for(int i =0; i< PROMPT_COUNT;i++){
-        printf("card with prompt: %s ",cards[i]->prompt_text);
-    }
+    
+    printf("card with prompt: %s \n",draw_random(cards)->prompt_text);
+
+    
+    // for(int i =0; i< 5;i++){
+    //     printf("card with prompt: %s ",cards[i]->prompt_text);
+    // }
+
+
     free_cards(cards);
     fclose(prompt_file);
+
+
+
 
     return 1; 
 }
