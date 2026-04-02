5297f1dc817afe6aef1f6fe6def321bd332461da
Kai Tano Bague <baguekai@teach.cs.toronto.edu>
2026-04-02 02:52:41 -0400

    small changes, moved some stuff around to make more sense

--- include/comms.h
+++ include/comms.h
@@ -2,6 +2,17 @@
 #define COMMS_H
 #include "gamestructs.h"
 
+typedef enum {
+    CLIENT_DISCONNECT = -2,
+    READ_FAIL = -1,
+    SEND_FAIL = -1,
+    READ_PARTIAL = 0,
+    READ_SUCCESS = 1,
+    SEND_SUCCESS = 1,
+    PARTIAL_READ_SUCCESS = 2,
+    TIMEOUT = 3
+} response;
+
 // TODO: move comment from comms.c to here abt returns
 int game_read(Player *p);
 
