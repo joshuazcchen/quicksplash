a7922514420a215e98207b4f57c19f94382859ad
Kai Tano Bague <baguekai@teach.cs.toronto.edu>
2026-04-02 16:17:50 -0400

    input is more general use

--- src/client/input.c
+++ src/client/input.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    char name[21]; // Buffer for 20 characters + 1 for null terminator
-    printf("Enter name (max 20 chars): ");
+char* get_text_input(int max_chars) {
+    char text[max_chars + 1]; // Buffer for max_chars characters + 1 for null terminator
+    printf("Enter text (max %d chars): ", max_chars);
     
-    if (fgets(name, sizeof(name), stdin)) { // fgets with sizeof(name) exactly gets 20 characters and null terminates the string aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        printf("You entered: %s", name);
-    }
-    return 0;
+    if (fgets(name, sizeof(text), stdin)) { // fgets with sizeof(name) exactly gets 20 characters and null terminates the string aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      return text;
+    else {
+      exit(1);
+  }
 }
 
