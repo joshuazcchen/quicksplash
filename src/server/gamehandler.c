d25812c3b348d0164c9fce1d0ffece4c6f24f626
erikhateshw <erik.dadashyan@gmail.com>
2026-04-03 16:26:20 -0400

    Updated makefile, added server test file wrote blueprint on gamehandler

--- src/server/gamehandler.c
+++ src/server/gamehandler.c
@@ -2,3 +2,19 @@
 #include "gamestructs.h"
 
 
+
+
+// all server-client communication goes on here 
+
+
+response game_loop(){
+    
+    // start round 
+    // send round prompt to players
+    // wait for all player responses 
+    // initiate voting 
+    // determine winner or round 
+    // end round 
+    // repeat if game isnt over 
+    return GAME_SUCCESS;
+}   
