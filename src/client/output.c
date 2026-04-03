508ae74ec9992d19b1a29fbe6cbc9cd6d85a8a63
Kai Tano Bague <baguekai@teach.cs.toronto.edu>
2026-04-03 19:51:53 -0400

    card centered on the screen

--- src/client/output.c
+++ src/client/output.c
@@ -29,31 +29,39 @@ void justify_text_format(char* text_format, int min_char, int max_char, char* te
   int i = 0;
   int len = strlen(text);
   do {
-    printf("│%-*.*s│\n", min_char, max_char, text + i);
+    printf(text_format, min_char, max_char, text + i);
     i += min_char;
   } while (i < len);
 }
 
 void show_card_prompt(Card card) {
-  int horizontal_size = terminal_width;
-  printf("╭");
+  int horizontal_size = terminal_width / 2;
+  int j = (terminal_width - horizontal_size) / 2;
+  char start_pad[j + 1];
+  memset(start_pad, ' ', j);
+  start_pad[j] = '\0';
+
+  printf("%s╭", start_pad);
   display_n_times("─", horizontal_size - 2);
   printf("╮\n");
 
   char* title = "NEW CARD";
   int left_pad = (horizontal_size - 2 - strlen(title)) / 2;
   int right_pad = (horizontal_size - 2 - strlen(title)) - left_pad;
-  printf("│");
+  printf("%s│", start_pad);
   display_n_times(" ", left_pad);
   printf("%s", title);
   display_n_times(" ", right_pad);
   printf("│\n");
 
   char* desc = "Type a funny response to this sentence:";
-  justify_text_format("│%-*.*s│\n", horizontal_size-2, horizontal_size-2, desc);
-  justify_text_format("│%-*.*s│\n", horizontal_size-2, horizontal_size-2, card.prompt_text);
+  char format[strlen(start_pad) + 10 + 1];
+  strcpy(format, start_pad);
+  strcat(format, "│%-*.*s│\n");
+  justify_text_format(format, horizontal_size-2, horizontal_size-2, desc);
+  justify_text_format(format, horizontal_size-2, horizontal_size-2, card.prompt_text);
 
-  printf("╰");
+  printf("%s╰", start_pad);
   display_n_times("─", horizontal_size-2);
   printf("╯\n");
 }
