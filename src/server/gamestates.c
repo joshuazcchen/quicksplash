ea52e3acfa0989b40d4383687801df6113f18547
erikhateshw <erik.dadashyan@gmail.com>
2026-04-03 20:09:36 -0400

    implemented reciving player prompt response

--- src/server/gamestates.c
+++ src/server/gamestates.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "server_comms.h"
+#include "protocol.h"
 extern Player players[LOBBY_SIZE];
 Card **cards;
 Card * drawn_card;
@@ -28,9 +30,26 @@ response await_responses() {
     drawn_card->responses = malloc(sizeof(Response *)*LOBBY_SIZE);
     for(int i = 0; i < LOBBY_SIZE; i++){
         drawn_card->responses[i] = malloc(sizeof(Response)); //allocate space to be filled later
-        // drawn_card->responses[i]->player = players[i]; // iterate through player array 
+        drawn_card->responses[i]->player = &(players[i]); // iterate through player array 
         drawn_card->responses[i]->response = NULL; 
     }
+
+    Packet prompt_message = ctop(*(drawn_card));
+    if(s_send(prompt_message) == GAME_SUCCESS){
+        printf("Sent messages to players \n");
+    }
+
+    if(s_listen(60) == TIMEOUT){ /// time limit to check for responses
+        printf("responses recorded and timeout \n"); 
+        for(int i = 0; i < LOBBY_SIZE; i++){ 
+            for(int j = 0; j < LOBBY_SIZE; j++){
+                if(drawn_card->responses[j]->player->p_id == players[i].p_id){
+                    drawn_card->responses[j]->response = players[i].active.data;
+                }
+            }
+        }
+    }
+
 
     // server goes here to receive player resposnses 
     return GAME_SUCCESS;
