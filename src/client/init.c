20fd687998ec6447053225c04ad654d1f9e6db78
joshua <joshuaz.chen@mail.utoronto.ca>
2026-04-04 00:30:08 -0400

    im not a graphic designer

--- src/client/init.c
+++ src/client/init.c
@@ -20,41 +20,73 @@ int main() {
 		exit(1);
 	}
 	// temporary, but gets the users name, the port they want to connect to and their address they want to connect to
-	printf("temporary: what name? (32):\n");
-	char name[32]; // how many characters do we want the maximum name legnth to be?
-	get_str_to_ptr(name, 32);
-	printf("temporary: what port? (7):\n");
+	clear_screen();
+	char name[32];
 	char port[7];
-	get_str_to_ptr(port, 7);
-	printf("temporary: what addr? (default 127.0.0.1):\n");
 	char s_address[30];
-	get_str_to_ptr(s_address, 30);
-	printf("\n");
-	// connects
-	response status = c_connect(strtol(port, NULL, 10), s_address);
+
+	server_select(name, port, s_address);
+	int s_port = strtol(port, NULL, 10);
+
+	response status = c_connect(s_port, s_address);
 	if (status == SEND_SUCCESS) {
-		Packet p = stop(P_JOIN, name); // once again we need a better name for this, string to packet.	
+		Packet p = stop(P_JOIN, name);
 		if (c_send(p) == SEND_SUCCESS) {
-			printf("great ok client ready\n");
+			printf("Joined as %s\n", name);
 		} else {
-			printf("bad\n");
+			printf("Error in sending join packet\n");
+			exit(1);
 		}
 		sleep(2);
 		Packet pst = stop(P_START, "orange");
 		if (c_send(pst) == SEND_SUCCESS) {
-			printf("started\n");
+			printf("started game\n");
 		} else {
-			printf("bad");
+			printf("bad packet");
 		}
 		while (1) {
 			sleep(1);
 			if (c_read() == READ_SUCCESS && ready) {
-				printf("%s\n", ptoc(&active).prompt_text);
+				Card rec = ptoc(&active);
+				printf("\ncard got: %s\n", rec.prompt_text);
 			}
 		}
-	} else {
-		printf("no\n");
-		exit(1);
 	}
+//	printf("temporary: what name? (32):\n");
+//	char name[32]; // how many characters do we want the maximum name legnth to be?
+//	get_str_to_ptr(name, 32);
+//	printf("temporary: what port? (7):\n");
+//	char port[7];
+//	get_str_to_ptr(port, 7);
+//	printf("temporary: what addr? (default 127.0.0.1):\n");
+//	char s_address[30];
+//	get_str_to_ptr(s_address, 30);
+//	printf("\n");
+//	// connects
+//	response status = c_connect(strtol(port, NULL, 10), s_address);
+//	if (status == SEND_SUCCESS) {
+//		Packet p = stop(P_JOIN, name); // once again we need a better name for this, string to packet.	
+//		if (c_send(p) == SEND_SUCCESS) {
+//			printf("great ok client ready\n");
+//		} else {
+//			printf("bad\n");
+//		}
+//		sleep(2);
+//		Packet pst = stop(P_START, "orange");
+//		if (c_send(pst) == SEND_SUCCESS) {
+//			printf("started\n");
+//		} else {
+//			printf("bad");
+//		}
+//		while (1) {
+//			sleep(1);
+//			if (c_read() == READ_SUCCESS && ready) {
+//				printf("%s\n", ptoc(&active).prompt_text);
+//			}
+//		}
+//	} else {
+//		printf("no\n");
+//		exit(1);
+//	}
 	return 0;
 }
