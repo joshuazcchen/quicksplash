6947cbe64e19e6ceef11479812726661fea6998b
joshua <joshuaz.chen@mail.utoronto.ca>
2026-04-04 16:30:35 -0400

    protocol should be done time to test

--- src/common/protocol.c
+++ src/common/protocol.c
@@ -6,33 +6,61 @@
 
 // idk if we need this to be an actual independent copy of the string, but i think it should be ok so long as the client never modifies it.
 char* ptos(Packet *p) {
-    p->data[BUFFERSIZE-1] = '\0';
+    //p->data[BUFFERSIZE-1] = '\0';
+	// woah no more set buffersize!!! ^^^ 
+	// if this is all i need i will eat a shoe but i do genuinely think this is all i need LOL.
     return p->data;
 }
 
 Card ptoc(Packet *p) {
     Card c;
     memset(&c, 0, sizeof(Card));
-    c.prompt_text = malloc(strlen(p->data) + 1);
-    strcpy(c.prompt_text, p->data);
+	// since the packets are funkier here but we also have an easy way to guarantee we have exactly the size we need,
+	// might as well check that we have a proper packet like we do everywhere else.
+	if (p->header.length > 0 && p->data != NULL) {
+		c.prompt_text = malloc(p->header.length);
+		strcpy(c.prompt_text, p->data);
+	}
     return c;
 }
 
 Packet ctop(Card c) {
     Packet p;
-    p.type = P_CARD;
-    memset(p.data, '\0', BUFFERSIZE);
-    strncpy(p.data, c.prompt_text, sizeof(char)*(strlen(c.prompt_text)+1));
-    printf("sending packet string: \n %s \n",p.data);
+    p.header.type = PKT_CARD;
+    memset(p.header, '\0', sizeof(PacketHeader));
+	p.header.length = strlen(c.prompt_text) + 1;
+	p.data = malloc(p.header.length);
+	strcpy(p.data, c.prompt_text);
+	printf("sending packet string: n %s \n", p.data);
     return p;
 }
 
 // surely theres a better name for this lmfao
-Packet stop(p_type type, char* c) {
+Packet stop(pkt_type type, char* c) {
     Packet p;
-    p.type = type;
-    memset(p.data, '\0', BUFFERSIZE);
-    strncpy(p.data, c, BUFFERSIZE);
-    p.data[BUFFERSIZE-1] = '\0';
+    p.header.type = type;
+    memset(p.header, '\0', sizeof(PacketHeader));
+	if (c != NULL && c[0] != '\0') { // verify that theres actually something to put in the string.
+		p.header.length = strlen(c) + 1;
+		p.data = malloc(p.header.length);
+		strcpy(p.data, c);
+	} else {
+		p.header.length = 22;
+		p.data = malloc(sizeof(char) * 22);
+		// stupid but i dont want to deal with empty packet lengths probably breaking stuff.
+		// i coded in a check in the server comms iirc but its like, why even bother risking
+		strcpy(p.data, "joshua/corgi was here");
+	}
     return p;
+}
+
+
+// because i dont wanna go back and change all the uses of my stupid naming im gonna just make aliases for now.
+// TODO: swap these to fn aliases in attribute of {*}.h
+Packet strtopkt(pkt_type type, char* c) {
+	return stop(type, c);
+}
+
+Packet ctopkt(Card c) {
+	return ctop(c);
 }
